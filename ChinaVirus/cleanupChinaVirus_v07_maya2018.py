b168b2610acbe9ae9b486baefed5a6fdd5713d0a
jang2029 <jang2029@gmail.com>
2023-04-06 17:11:37 +0900

    update

--- ChinaVirus/cleanupChinaVirus_v07_maya2018.py
+++ ChinaVirus/cleanupChinaVirus_v07_maya2018.py
@@ -1,14 +1,19 @@
 
-import os, sys
+import os, sys, csv
 import subprocess
 import time
 import multiprocessing
 
-def scan(file):
 
+
+
+def scan(file):
+    
     print('PID :', os.getpid())
-    subprocess.run(file)
     print (f'process: {file}')
+    subprocess.run(file)
+    
+
 
 
 def list_chuck(arr, n):
@@ -16,7 +21,7 @@ def list_chuck(arr, n):
 
 
 def serchMayafile(dirname, mayaVersion):    
-
+    
     subprocess.run(f'"C:/Program Files/Autodesk/{mayaVersion}/bin/mayabatch.exe" -command "loadPlugin MayaScanner; loadPlugin MayaScannerCB;"')
     list = []
     for (path, dir, files) in os.walk(dirname):
@@ -24,7 +29,7 @@ def serchMayafile(dirname, mayaVersion):
             ext = os.path.splitext(filename)[-1]
             if (ext == '.ma') or (ext == '.mb'):
                 mayaFile = path.replace('\\', '/') + '/' + filename
-                list.append("C:/Program Files/Autodesk/" + mayaVersion + "/bin/mayabatch" + " " + mayaFile)
+                list.append("C:/Program Files/Autodesk/" + mayaVersion + "/bin/maya.exe -batch -file " + mayaFile)
     print (list)
 
     result_array = list_chuck(list, 16)
@@ -47,21 +52,21 @@ def main():
     dirname = input('Type serch folder:')
     if not os.path.isdir(dirname):
         print('\n\n')
-        print('\033[95m'+'wrong Path!!' + '\033[0m')
+        print('wrong Path!!')
         sys.exit(0)
 
 
     # mayaVersion = input('Type maya version( Maya2018, Maya2022):')
-    mayaVersion = 'Maya2022'
+    mayaVersion = 'Maya2018'
     startTime = time.time()
     if (mayaVersion == 'Maya2022') or (mayaVersion == 'Maya2018'):
         serchMayafile(dirname, mayaVersion)
     else:
-        print('\033[95m'+'wrong version!!' + '\033[0m')
+        print('wrong version!!')
         sys.exit(0)
 
-    print('\033[96m' + f"Total takes Time {int(time.time()-int(startTime))} second" + '\033[0m')
-
+    print(f"Total takes Time {int(time.time()-int(startTime))} second")
+    
 
 
 if __name__ == '__main__':
