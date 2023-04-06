626c904abeecdd2fd28e1a2002c0e9bda9d72a0b
jang2029 <jang2029@gmail.com>
2023-04-06 10:52:20 +0900

    update

--- cleanupChinaVirus_v04.py
+++ cleanupChinaVirus_v04.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 import subprocess
 import time
 import multiprocessing
@@ -8,8 +8,11 @@ import multiprocessing
 
 
 
+def list_chuck(arr, n):
+    return [arr[i: i + n] for i in range(0, len(arr), n)]
 
-def search(dirname):
+
+def serchMayafile(dirname, mayaVersion):
     
     list = []
     for (path, dir, files) in os.walk(dirname):
@@ -17,16 +20,21 @@ def search(dirname):
             ext = os.path.splitext(filename)[-1]
             if (ext == '.ma') or (ext == '.mb'):
                 mayaFile = path.replace('\\', '/') + '/' + filename
-                list.append("C:/Program Files/Autodesk/Maya2022/bin/mayabatch" + " " + mayaFile)
-    print (list)            
-    procs = []
-    for i in list:
-        p = multiprocessing.Process(target=scan, args=(i, ))
-        p.start()
-        procs.append(p)
+                list.append("C:/Program Files/Autodesk/" + mayaVersion + "/bin/mayabatch" + " " + mayaFile)
+    print (list)
 
-    for p in procs:
-        p.join()
+    result_array = list_chuck(list, 16)
+    print (result_array)
+    print (len(result_array))
+    for a in result_array:
+        procs = []
+        for i in a:
+            p = multiprocessing.Process(target=scan, args=(i, ))
+            p.start()
+            procs.append(p)
+
+        for p in procs:
+            p.join()
 
 def scan(file):
 
@@ -37,8 +45,19 @@ def scan(file):
 
 
 if __name__ == '__main__':
-    filePath = r'D:\projects\eaapexseason17_42048P\assets\3D\character\ash\model\output'
-    startTime = time.time()
-    search(filePath)
-    print(f"Total takes Time {int(time.time()-int(startTime))} second")
 
+    dirname = input('Type serch folder:')
+    if not os.path.isdir(dirname):
+        print('\033[95m'+'wrong Path!!' + '\033[0m')
+        sys.exit(0)
+
+
+    mayaVersion = input('Type maya version( Maya2018, Maya2022):')
+    startTime = time.time()
+    if (mayaVersion == 'Maya2022') or (mayaVersion == 'Maya2018'):
+        serchMayafile(dirname, mayaVersion)
+    else:
+        print('\033[95m'+'wrong version!!' + '\033[0m')
+        sys.exit(0)
+
+    print('\033[96m' + f"Total takes Time {int(time.time()-int(startTime))} second" + '\033[0m')
